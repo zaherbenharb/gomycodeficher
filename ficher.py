26d13ffb1d6a0bd514a9eeadc9b7952106907117
Ben Harb <96679889+zaherbenharb@users.noreply.github.com>
2022-02-21 02:49:05 -0800

    Add files via upload

---
+++ ficher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+
+
+
+# In[21]:
+
+
+f = open('python.txt','r',encoding = 'utf-8')
+
+f.read(4)
+
+
+# In[22]:
+
+
+f.read()
+
+
+# In[23]:
+
+
+lignes = f.readlines()
+
+
+# In[24]:
+
+
+for ligne in lignes:
+      print(ligne)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
